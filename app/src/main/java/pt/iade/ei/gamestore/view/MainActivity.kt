833bb707cb766157fa1ba174ba4e0f35e93bff48
macazava <macazava@gmail.com>
2025-11-25 21:31:39 +0000

    coloquei dentro de uma box o icone (imagem) feautured para aparecer em destaque como se estivesse selecionado

--- app/src/main/java/pt/iade/ei/gamestore/view/MainActivity.kt
+++ app/src/main/java/pt/iade/ei/gamestore/view/MainActivity.kt
@@ -5,13 +5,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -88,12 +91,21 @@ fun MainScreen(games: List<Game>, onGameClick: (Game) -> Unit) {
                     selected = false, // ícone estático
                     onClick = {},     // sem ação
                     icon = {
-                        Image(
-                            painter = painterResource(id = R.drawable.icon_featured),
-                            contentDescription = "Featured",
-                            modifier = Modifier.size(24.dp)
-                        )
-                    },
+                        Box(
+                            modifier = Modifier
+                                .size(32.dp) // espaço maior que o ícone
+                                .clip(RoundedCornerShape(8.dp))
+                                .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.1f)), // fundo suave
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.icon_featured),
+                                contentDescription = "Featured",
+                                modifier = Modifier.size(24.dp)
+                            )
+                        }
+                    }
+                    ,
                     label = { Text("Featured") }
                 )
                 NavigationBarItem(
