27496ff57d6e0204974f64efe5b27ef551f37ff7
macazava <macazava@gmail.com>
2025-11-26 12:40:28 +0000

    foram alterados os modelos para ficarem preparados para futuramente receberem dados do backend - passam agora a receber no atributo imagem uma URL falsa

--- app/src/main/java/pt/iade/ei/gamestore/model/Game.kt
+++ app/src/main/java/pt/iade/ei/gamestore/model/Game.kt
@@ -3,7 +3,7 @@ package pt.iade.ei.gamestore.model
 data class Game(
     val id: Int,
     val name: String,
-    val imageResId: Int,
+    val imageUrl: String, //agora vai ser uma URL falsa
     val description: String,
     val items: List<Item>//Permite carregar os itens compraveis na GameDetailActivity
 )
--- app/src/main/java/pt/iade/ei/gamestore/model/Item.kt
+++ app/src/main/java/pt/iade/ei/gamestore/model/Item.kt
@@ -3,7 +3,7 @@ package pt.iade.ei.gamestore.model
 data class Item(
     val id: Int,
     val name: String,
-    val imageResId: Int,
+    val imageUrl: String, //agora vai ser uma URL falsa
     val description: String,
     val price: Double
 )
